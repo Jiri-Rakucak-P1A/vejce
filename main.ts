4b601bbc85c51c44f157c3e82ed0eb3d8ed786cd
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-02-09 11:30:11 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+/*
+
+radio.setGroup(2)
+let start = false
+let start2 = false
+
+music.setVolume(225)
+music.changeTempoBy(20)
+
+input.onButtonPressed(Button.A, function() {
+    radio.sendValue("kod", 15)
+    start = true
+
+
+radio.onReceivedValue(function(name: string, value: number) {
+    
+    if (name == "kod"){
+        if (value = 15) {
+            start2 = true
+            
+        }
+    }
+
+})
+})
+if (start && start2) {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        basic.showString("READY!")
+        basic.pause(10)
+        basic.showString("GO!")
+    music.stopMelody(MelodyStopOptions.All)
+}
+*/
+let x =0
+let y = 0
+let z = 0
+input.setAccelerometerRange(AcceleratorRange.TwoG)
+basic.forever(function() {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    z = input.acceleration(Dimension.Z)
+    console.logValue("x",x)
+    //console.logValue("y",y)
+    //console.logValue("z",z)
+
+
+
+
+    basic.pause(150)
+})
